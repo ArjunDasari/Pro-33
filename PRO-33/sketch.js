4f827e887a4032b92fdf9c6b0d8a4a0e1efcddcb
ArjunDasari <73619030+ArjunDasari@users.noreply.github.com>
2021-11-01 16:55:53 -0400

    Add files via upload

---
+++ PRO-33/sketch.js
@@ -0,0 +1,17 @@
+var bgImg= "snow1.jpg";
+var backgroundImg= "backgroundImg.png";
+
+
+function preload(){
+
+}
+function setup() {
+  createCanvas(800,400);
+
+}
+
+function draw() {
+  background(230); 
+  drawSprites(); 
+
+}
